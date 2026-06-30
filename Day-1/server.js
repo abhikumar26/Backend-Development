818d50e724c32adcd0c3d6c186231d793ec4b988
abhikumar26 <abhinavkumarprithvi2gmail.com>
2026-06-30 12:30:16 +0530

    change

--- Day-1/server.js
+++ Day-1/server.js
@@ -1,16 +1,16 @@
 import http from 'http';
-const port = 4000
+const port = 4000;
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
         res.end("<h1>Welcome to backend server</h1>")
-    // } else if(req.url){
-    //     res.end('<h1>This is about Page</h1>')
-    // } else if(req.url){
-    //     res.end('<h1>This is contact Page</h1>')
-    // } else if(req.url){
-    //     res.end('<h1>This is a Home page</h1>')
-    // } else{
-    //     res.end('<h1>No data found!</h1>')
+    } else if(req.url == '/about'){
+        res.end('<h1>This is about Page</h1>')
+    } else if(req.url == '/contact/mobilesss'){
+        res.end('<h1>This is contact Page</h1>')
+    } else if(req.url == '/homeee'){
+        res.end('<h1>This is a Home page</h1>')
+    } else{
+        res.end('<h1>No data found!</h1>')
     }
 })
 server.listen(port,()=>{
